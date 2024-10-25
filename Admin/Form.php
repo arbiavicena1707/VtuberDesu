71a540740aa8bf56b5f90044e9fde302d1e19796
arbiavicena1707 <arbi.avicena2020@gmail.com>
2024-10-25 10:04:01 +0700

    autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%

---
+++ Admin/Form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin VtuberDesu</title>
+</head>
+<body>
+    <form action="" enctype="multipart/form-data">
+        <!-- Inputikan Nama # -->
+         <!-- Inputkan NISN -->
+          <!-- Inputkan NIS -->
+    </form>
+</body>
+</html>
